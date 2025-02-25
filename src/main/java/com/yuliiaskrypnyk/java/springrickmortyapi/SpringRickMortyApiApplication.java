c5da08e4857a3d0e0346fc0e28a52ca9ba87b0f5
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-02-25 10:54:59 +0100

    Initial commit

---
+++ src/main/java/com/yuliiaskrypnyk/java/springrickmortyapi/SpringRickMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.yuliiaskrypnyk.java.springrickmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRickMortyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRickMortyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuliiaskrypnyk/java/springrickmortyapi/SpringRickMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuliiaskrypnyk.java.springrickmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRickMortyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
